234de68f82436ce7ad930bfb376acd322017f788
Pavlo Volodymyrov <pavlo_volodymyrov@epam.com>
2022-03-03 17:24:23 +0200

    markdown fix

--- get-data.ps1
+++ get-data.ps1
@@ -1,9 +1,9 @@
 $header = '
-#2022 Russian invasion of Ukraine
-##Approximate loses
+# 2022 Russian invasion of Ukraine
+## Approximate loses
 '
 $footer ='
-##Source [Wikipedia] (https://uk.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B5_%D0%B2%D1%82%D0%BE%D1%80%D0%B3%D0%BD%D0%B5%D0%BD%D0%BD%D1%8F_%D0%B2_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%83_(2022)#%D0%92%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%BE%D0%B2%D1%96_%D0%B2%D1%82%D1%80%D0%B0%D1%82%D0%B8_%D0%BE%D0%BA%D1%83%D0%BF%D0%B0%D0%BD%D1%82%D1%96%D0%B2)
+## Source [Wikipedia] (https://uk.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B5_%D0%B2%D1%82%D0%BE%D1%80%D0%B3%D0%BD%D0%B5%D0%BD%D0%BD%D1%8F_%D0%B2_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%83_(2022)#%D0%92%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%BE%D0%B2%D1%96_%D0%B2%D1%82%D1%80%D0%B0%D1%82%D0%B8_%D0%BE%D0%BA%D1%83%D0%BF%D0%B0%D0%BD%D1%82%D1%96%D0%B2)
 '
 
 
